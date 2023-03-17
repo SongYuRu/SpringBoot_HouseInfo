23ddcb50326e90106cd5ec6875f5ffcefa91a389
SongYuRu <128151568+SongYuRu@users.noreply.github.com>
2023-03-17 20:53:58 +0800

    Changes

--- src/main/java/com/example/springboot_houseinfo/service/RegisterService.java
+++
@@ -1,7 +0,0 @@
-package com.example.springboot_houseinfo.service;
-
-import com.example.springboot_houseinfo.pojo.Register;
-
-public interface RegisterService {
-    Register registerLogin(String usernames, String passwords);
-}
